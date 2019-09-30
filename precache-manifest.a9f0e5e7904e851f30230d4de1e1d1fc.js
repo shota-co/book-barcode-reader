b789ab3ad423fea593253c153613a5d892241d47
shota-co <dripcoffee1018@gmail.com>
2019-09-30 23:35:31 +0900

    Updates

---
+++ precache-manifest.a9f0e5e7904e851f30230d4de1e1d1fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf36b2a7803c78f3883f5779ca6651c0",
+    "url": "/book-barcode-reader/index.html"
+  },
+  {
+    "revision": "d2e555c8fbdb309aa79f",
+    "url": "/book-barcode-reader/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3634dd15bb0e2311f395",
+    "url": "/book-barcode-reader/static/js/2.df6d562f.chunk.js"
+  },
+  {
+    "revision": "d2e555c8fbdb309aa79f",
+    "url": "/book-barcode-reader/static/js/main.5707b8cb.chunk.js"
+  },
+  {
+    "revision": "4235d9ff61172add890a",
+    "url": "/book-barcode-reader/static/js/runtime-main.2d837fa5.js"
+  }
+]);
